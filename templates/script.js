e7917872252ab6d6f35bedb37dae9a9f04e182f6
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2024-11-27 04:52:47 +0000

    main page

---
+++ templates/script.js
@@ -0,0 +1,25 @@
+// Обработка кнопок
+document.getElementById("search-tor").addEventListener("click", () => {
+  const query = document.getElementById("search").value;
+  if (query) {
+    window.open(`/search?query=${encodeURIComponent(query)}`, "_blank");
+  } else {
+    alert("Введите запрос для поиска.");
+  }
+});
+
+document.getElementById("open-tiktok").addEventListener("click", () => {
+  window.open("/redirect/tikto", "_blank");
+});
+
+document.getElementById("open-instagram").addEventListener("click", () => {
+  window.location.assign("/redirect/instagram", "_blank");
+});
+
+document.getElementById("open-2ip-tor").addEventListener("click", () => {
+  window.open("/redirect/2ip", "_blank"); // Перенаправление на 2ip через TOR
+});
+
+document.getElementById("open-2ip-vpn").addEventListener("click", () => {
+  window.open("/open_2ip_vpn", "_blank"); // Перенаправление на 2ip через VPN
+});
