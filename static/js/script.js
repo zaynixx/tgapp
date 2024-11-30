c470d5d9c9c70a7c4919d81b80d6d2b6278c55a2
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2024-11-30 02:29:51 +0000

    register + login pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,108 @@
+let loggedIn;
+
+fetch("/me", {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.username) {
+      loggedIn = true;
+      sessionStorage.setItem("user", JSON.stringify(data));
+    }
+  })
+  .catch((error) => {
+    sessionStorage.removeItem("user");
+    loggedIn = false;
+    console.log(error);
+  });
+
+const closeLogin = () => {
+  const wrapper = document.getElementById("login-drawer");
+  const drawer = wrapper.querySelector("div");
+
+  wrapper.style.backdropFilter = "blur(0px)";
+  wrapper.style.opacity = 0;
+  drawer.style.transform = "translateY(100%)";
+
+  setTimeout(() => {
+    wrapper.style.display = "none";
+    wrapper.style.backdropFilter = "";
+    wrapper.style.opacity = "";
+    drawer.style.transform = "";
+  }, 300);
+};
+
+const openLogin = () => {
+  if (typeof loggedIn === "undefined") return;
+  const wrapper = document.getElementById("login-drawer");
+  const drawer = wrapper.querySelector("div");
+
+  wrapper.style.backdropFilter = "";
+  wrapper.style.opacity = "";
+  drawer.style.transform = "";
+
+  wrapper.onclick = () => closeLogin();
+  drawer.onclick = (e) => e.stopPropagation();
+
+  wrapper.style.display = "block";
+};
+
 // Обработка кнопок
 document.getElementById("search-tor").addEventListener("click", () => {
+  if (!loggedIn) {
+    openLogin();
+    return;
+  }
   const query = document.getElementById("search").value;
   if (query) {
-    window.open(`/search?query=${encodeURIComponent(query)}`, "_blank");
+    window.location.assign(`/search?query=${encodeURIComponent(query)}`);
   } else {
     alert("Введите запрос для поиска.");
   }
 });
 
 document.getElementById("open-tiktok").addEventListener("click", () => {
-  window.open("/redirect/tiktok", "_blank");
+  if (!loggedIn) {
+    openLogin();
+    return;
+  }
+  window.location.assign("/redirect/tiktok");
 });
 
 document.getElementById("open-instagram").addEventListener("click", () => {
-  window.location.assign("/redirect/instagram", "_blank");
+  if (!loggedIn) {
+    openLogin();
+    return;
+  }
+  window.location.assign("/redirect/instagram");
 });
 
 document.getElementById("open-2ip-tor").addEventListener("click", () => {
-  window.open("/redirect/2ip", "_blank"); // Перенаправление на 2ip через TOR
+  if (!loggedIn) {
+    openLogin();
+    return;
+  }
+  window.location.assign("/redirect/2ip"); // Перенаправление на 2ip через TOR
 });
 
 document.getElementById("open-2ip-vpn").addEventListener("click", () => {
-  window.open("/open_2ip_vpn", "_blank"); // Перенаправление на 2ip через VPN
+  if (!loggedIn) {
+    openLogin();
+    return;
+  }
+  window.location.assign("/open_2ip_vpn"); // Перенаправление на 2ip через VPN
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.querySelectorAll('nav button');
+  const navLinks = document.querySelectorAll("nav button");
 
   navLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-      document.body.setAttribute('tab', link.getAttribute('data-tab'));
+    link.addEventListener("click", () => {
+      if (!loggedIn) {
+        openLogin();
+        return;
+      }
+      document.body.setAttribute("tab", link.getAttribute("data-tab"));
     });
   });
 });
